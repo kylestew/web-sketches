e96bdee59bd85dbdb84b3d1720e8562ac32e6ac8
Kyle Stewart <kylestew@gmail.com>
2022-04-18 13:26:46 -0600

    Refined enough for the first showing

--- circlestudy/sketch.js
+++ circlestudy/sketch.js
@@ -3,6 +3,7 @@ const colors = require("riso-colors");
 const { randomInt, randomRange } = require("../snod/random");
 const { randomInArray } = require("../snod/arrays");
 const { linspace } = require("../snod/math");
+const { luminosity, hexToRGB } = require("../snod/color");
 const draw = require("../snod/draw");
 
 const settings = {
@@ -30,15 +31,37 @@ Multiple Circles:
 */
 
 const background = randomInArray(colors);
-const primary = randomInArray(colors);
+// try to ensure enough contrast between colors
+let primary = randomInArray(colors);
+while (
+  Math.abs(
+    luminosity(hexToRGB(background.hex)) - luminosity(hexToRGB(primary.hex))
+  ) < 0.3
+) {
+  primary = randomInArray(colors);
+}
+
+const densityShapingFn = () => {
+  const rand = Math.random();
+  if (rand < 0.4) {
+    return (x) => -x * x + 1;
+  } else if (rand < 0.6) {
+    return (x) => -x * x * x + 1;
+  } else if (rand < 0.8) {
+    return (x) => -x * x * x * x + 1;
+  }
+  return (x) => x;
+};
 
 const genCircleMatt = (count, rotations) => {
   // angle
   const thetas = linspace(0, 2 * Math.PI * rotations, count, true);
+
   // distance from center
-  // let's vary that density baby
-  const radii = linspace(0, 1, count, true).map((val) => -val * val + 1);
-  // const radii = linspace(0, 1, count, true).map((val) => Math.log(val + 1));
+  let radii = linspace(0, 1, count, true);
+
+  // shape the radial density
+  radii = radii.map(densityShapingFn());
 
   return thetas.map((theta, i) => {
     const r = radii[i];
@@ -58,19 +81,25 @@ const sketch = () => {
     context.scale(0.8, 0.8);
 
     // gen discs in circle
-    let count = parseInt(randomRange(9000, 24000));
-    let circs = genCircleMatt(count, 1024);
+    let count = parseInt(randomRange(3200, 32000));
+    // let count = 20000;
+    let rotations = parseInt(randomRange(32, 512));
+    let circs = genCircleMatt(count, rotations);
 
-    // remove some
+    // randomly remove some
+    let dice = Math.random() * 0.1;
+    circs = circs.filter((_) => Math.random() > dice);
 
     circs.forEach((circ) => {
       // vary monochrome opacity
-      const opacity = parseInt(randomRange(1, 120));
+      const opacity = parseInt(
+        randomRange(randomRange(0, 20), randomRange(80, 164))
+      );
       const hex = primary.hex + opacity.toString(16);
       context.fillStyle = hex;
 
       // vary radii
-      let r = randomRange(0.004, 0.018);
+      let r = randomRange(randomRange(0.002, 0.006), randomRange(0.012, 0.022));
 
       draw.circle(context, circ, r);
     });
